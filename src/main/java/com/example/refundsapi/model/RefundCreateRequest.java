29fcf1f9a958aaaf1c97adf5f86ac5bd8bdbf002
HItesh Kumar Sharma <demo@example.com>
2026-04-17 11:49:10 +0000

    Initial Spring Boot API commit

---
+++ src/main/java/com/example/refundsapi/model/RefundCreateRequest.java
@@ -0,0 +1,21 @@
+package com.example.refundsapi.model;
+
+
+
+import lombok.Data;
+
+
+
+@Data
+
+public class RefundCreateRequest {
+
+
+
+    private double amount;
+
+    private String currency;
+
+    private String reason;
+
+}
